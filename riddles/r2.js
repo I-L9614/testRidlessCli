de29e2c011ac5112ea666525529ca9de57728d10
I-L9614 <leviitamar034@gmail.com>
2025-12-04 10:09:13 +0200

    add r2

---
+++ riddles/r2.js
@@ -0,0 +1,7 @@
+export default {
+    id:2,
+    name:'people',
+    difficulty:'MEDIUM',
+    taskDescripaion:'Who is the Prime Minister of Israel today?',
+    correctAnswer:'Benjamin Netanyahu'
+}
