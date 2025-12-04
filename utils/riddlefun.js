1a8df8e9d24e5b6117bf6ec73f7984ed78b91282
I-L9614 <leviitamar034@gmail.com>
2025-12-04 11:41:00 +0200

    add askRiddle function as a variable

---
+++ utils/riddlefun.js
@@ -0,0 +1,20 @@
+export const askRiddle = function(riddleObj) {
+    console.log(riddleObj.name)
+    console.log(riddleObj.taskDescripaion)
+    if(riddleObj.choices){
+        console.log(riddleObj.choices)
+        let choise = input('enter your choise number ro field to see choices as bumbers')
+        if(choise==='field') {
+            console.log(`0:${riddleObj.choices[0]},1:${riddleObj.choices[1]},2:${riddleObj.choices[2]}`)
+            choise = input('enter your choise number!')
+        }else{while(!choise===riddleObj.correctAnswer){
+            choise = input('wrong enswer.try again')
+        }}
+    }else{
+        let choise = input('enter your answer')
+        while(!choise===riddleObj.correctAnswer){
+            choise = input('wrong enswer.try again')  
+        }
+    }
+             
+}
